1ee4ae5c5c187fbf4d958fe452347f3b5c804d97
Briana <bsisler81@gmail.com>
2025-09-30 15:26:53 -0400

    Moved Node class to ThingList

--- DotChaser.java
+++ DotChaser.java
@@ -19,16 +19,6 @@ public class DotChaser {
     public boolean isTypeB;
   }
 
-  /**
-   * YOU'LL NEED TO PUT THIS SOMEWHERE ELSE
-   * HINT: WOULDN'T IT BE NICE TO HAVE A LIST OR QUEUE SO THAT
-   *       WE DON'T HAVE TO USE NODES HERE?
-   * This class is for linked lists of Thing's
-   */
-  public static class Node {
-    public Thing data;
-    public Node  next;
-  }
 
   // EEEEEK! STATIC METHODS!!! PLEASE FIND THEM A BETTER HOME.
   public static void rightTurn(Thing t) {
--- ThingList.java
+++ ThingList.java
@@ -1,7 +1,13 @@
 public class ThingList {
     
 }
-
+/**
+   * YOU'LL NEED TO PUT THIS SOMEWHERE ELSE
+   * HINT: WOULDN'T IT BE NICE TO HAVE A LIST OR QUEUE SO THAT
+   *       WE DON'T HAVE TO USE NODES HERE?
+   * This class is for linked lists of Thing's
+   */
 private class Node {
-    
+    public Thing data;
+    public Node next;
 }
