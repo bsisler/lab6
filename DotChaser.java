b544a27877a50cbd89b84eeadcb6260bd1ad47b3
Briana <bsisler81@gmail.com>
2025-10-04 14:55:16 -0400

    Moved Left, Right, and Maybe turn and step to Thing.Java

--- DotChaser.java
+++ DotChaser.java
@@ -9,25 +9,10 @@ public class DotChaser {
    * and, afterward, takes a step forward.  A TypeB Thing
    * only considers making a random turn every 10th round.
    *
-   * A STATIC CLASS? OH NO! GET IT OUT OF HERE!
    */
-  public static class Thing {
-    // dir: 0=North, 1=East, 2=South, 3=West.
-    // timeSinceLast: this is only important for "TypeB" Things.
-    public int  row, col, dir, timeSinceLast;
-    public char lab = 'r';
-    public boolean isTypeB;
-  }
 
 
   // EEEEEK! STATIC METHODS!!! PLEASE FIND THEM A BETTER HOME.
-  public static void rightTurn(Thing t) {
-    t.dir = (t.dir + 1) % 4;
-  }
-
-  public static void leftTurn(Thing t) {
-    t.dir = (t.dir + 3) % 4;
-  }
 
   public static void maybeTurn(Thing t) {
     int i = rand.nextInt(3);
@@ -55,16 +40,6 @@ public class DotChaser {
         leftTurn(t);
       }
     }
-  }
-
-  public static void step(Thing t) {
-    final int[] dc = {
-      0, 1, 0, -1
-    }, dr = {
-      1, 0, -1, 0
-    };
-    t.row += dr[t.dir];
-    t.col += dc[t.dir];
   }
 
   
--- Thing.java
+++ Thing.java
@@ -7,7 +7,14 @@ public abstract class Thing {
     protected int timeSinceLast;
     protected char. lab;
     protected Random rand;
-
+    
+    /*
+    * A "Thing" moves in a grid world. A TypeA Thing randomly
+    * decides to turn left or right (or not turn) every "round",
+    * and, afterward, takes a step forward.  A TypeB Thing
+    * only considers making a random turn every 10th round.
+    */
+    
     public Thing(int row, int col, char lab) {
         this.row = row;
         this.col = col;
@@ -15,5 +22,27 @@ public abstract class Thing {
         this.dir = 0;
         this.timeSinceLast = 0;
         this.rand = new Random(System.currentTimeMillis());
+    }
+
+    //Turn Right
+    public void rightTurn() {
+        dir = (dir + 1) % 4;
+    }
+
+    //Turn Left
+    public void leftTurn() {
+        dir = (dir + 3) % 4;
+    }
+
+    public abstract void maybeTurn();
+
+    //Move one step
+    public void step() {
+        //Row
+        int[] dr = {1, 0, -1, 0};
+        //Col
+        int[] dc = {0, 1, 0, -1};
+        row = row + dr[dir];
+        col = col + dc[dir];
     }
 }
