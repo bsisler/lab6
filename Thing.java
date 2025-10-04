cc3110d3224b7de50e512a27cf3614a73578409d
Briana <bsisler81@gmail.com>
2025-10-04 14:42:39 -0400

    Created the structre for Thing.java

--- Thing.java
+++ Thing.java
@@ -1,3 +1,19 @@
-public class Thing {
-    
+import java.util.Random;
+
+public abstract class Thing {
+    protected int row;
+    protected int col;
+    protected int dir;
+    protected int timeSinceLast;
+    protected char. lab;
+    protected Random rand;
+
+    public Thing(int row, int col, char lab) {
+        this.row = row;
+        this.col = col;
+        this.lab = lab;
+        this.dir = 0;
+        this.timeSinceLast = 0;
+        this.rand = new Random(System.currentTimeMillis());
+    }
 }
