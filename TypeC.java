166eb7fdee658452c3247212fcd14a8415fe54e5
Briana <bsisler81@gmail.com>
2025-10-04 15:14:25 -0400

    Created TypeC; diagonal movement

--- TypeC.java
+++ TypeC.java
@@ -1,3 +1,14 @@
-public class TypeC {
-    
+public class TypeC extends Thing{
+    public TypeC (int row, int col, char lab) {
+        super(row, col, lab);
+    }
+    private boolean diagonal = true;
+    public void maybeTurn() {
+        if (diagonal == true) {
+            leftTurn();
+        } else {
+            rightTurn();
+        }
+        diagonal = !diagonal;
+    }
 }
